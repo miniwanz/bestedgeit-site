0153c0f36cca9be66164e84cd461256c40c4a9d9
Aaron <awansley@gmail.com>
2014-02-13 19:43:36 -0800

    changes to config and gemfile

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 
 */
 
-jQuery(function($){$("#Phone").mask("(999) 999-9999? x99999",{placeholder:" "});});/* Author: 
+jQuery(function($){$("#Phone").mask("(999) 999-9999",{placeholder:" "});});/* Author: 
 
 */
 
